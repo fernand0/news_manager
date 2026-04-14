d3c9d932dcab230e18f05494cb8fdc06d6f8cdee
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2026-04-14 10:10:49 +0200

    Change of llm selection

--- news_manager/llm.py
+++ news_manager/llm.py
@@ -63,7 +63,8 @@ class GeminiClient(LLMClient):
 
         try:
             genai.configure(api_key=api_key)
-            self.model = genai.GenerativeModel('gemini-2.5-flash')
+            self.model = genai.GenerativeModel('gemini-3-flash-preview')
+            #self.model = genai.GenerativeModel('gemini-2.5-flash')
             #self.model = genai.GenerativeModel('gemini-2.0-flash-lite')
         except Exception as e:
             raise ConfigurationError(
